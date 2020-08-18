6cf14823efe1c11480c3f2e97f0756976fd95119
edensongjbs <edensongjbs@gmail.com>
2020-08-18 17:35:53 -0400

    change endpoints for heroku api

--- src/App.js
+++ src/App.js
@@ -6,9 +6,13 @@ import Header from './Components/Header'
 import GameContainer from './Containers/GameContainer'
 import Welcome from './Components/Welcome'
 
-const gamesURL = "http://localhost:3000/games"
-const movesURL = "http://localhost:3000/moves"
-const playersURL = "http://localhost:3000/players"
+// const gamesURL = "http://localhost:3000/games"
+// const movesURL = "http://localhost:3000/moves"
+// const playersURL = "http://localhost:3000/players"
+
+const gamesURL = "https://shobu-game-backend.herokuapp.com/games"
+const movesURL = "https://shobu-game-backend.herokuapp.com/moves"
+const playersURL = "https://shobu-game-backend.herokuapp.com/players"
 
 class App extends React.Component {
 
@@ -118,6 +122,7 @@ class App extends React.Component {
           playerTwoPiecesOut: [],
         })
         window.history.pushState({pathname: '/'}, "", '/')
+        window.location.reload()
     })
   } 
 
